1314ff2abf65e3fcce4ddea56df74842d8a7884e
SophieZ <sophiezinda17@gmail.com>
2019-10-26 09:50:07 -0700

    update

--- wp-content/themes/accelerate-theme-child/archive-case_studies.php
+++ wp-content/themes/accelerate-theme-child/archive-case_studies.php
@@ -11,33 +11,38 @@
  * @subpackage Accelerate Marketing
  * @since Accelerate Marketing 2.0
  */
+
 get_header(); ?>
 
-	<div id="primary" class="site-content">
+	<div id="primary" class="site-content sidebar">
 		<div class="main-content" role="main">
-		<?php while ( have_posts() ) : the_post();
-        $image_1 = get_field("image_1");
-        $size = "full";
-        $services= get_field('services');?>
+			<?php while ( have_posts() ) : the_post();
+      $image_1 = get_field("image_1");
+      $size = "full";
+      $services = get_field('services');
 
-    <article class="case-study">
+      ?>
+  <article class="case-study">
         <aside class="case-study-sidebar">
-            <h2><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
-            <h5><?php echo $services; ?></h5>
-            <?php the_excerpt(); ?>
-            <p><strong><a href="<?php the_permalink(); ?>">View Project</a></strong></p>
-          </aside>
-          <div class="case-study-images">
-            <a href="<?php the_permalink(); ?>">
-                <?php if($image_1) {
-                echo wp_get_attachment_image( $image_1, $size );
-              } ?>
-            </a>
+          <h2><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
+          <h4><?php echo $services; ?></h4>
+          <?php the_excerpt(); ?>
+          <p class="read-more-link"><a href="<?php the_permalink(); ?>">View Project</a></p>
+        </aside>
+
+        <div class="case-study-images">
+          <a href="<?php the_permalink(); ?>">
+              <?php if($image_1) {
+                  echo wp_get_attachment_image( $image_1, $size );
+                } ?>
+          </a>
         </div>
       </article>
-<?php endwhile; // end of the loop. ?>
+				<?php the_content(); ?>
+			<?php endwhile; // end of the loop. ?>
+
 
 		</div><!-- .main-content -->
-</div><!-- #primary -->
+	</div><!-- #primary -->
 
 <?php get_footer(); ?>
--- wp-content/themes/accelerate-theme-child/functions.php
+++ wp-content/themes/accelerate-theme-child/functions.php
@@ -9,15 +9,16 @@
 * @subpackage Accelerate Marketing
 * @since Accelerate Marketing 2.0
 */
+
 // Enqueue scripts and styles
 function accelerate_child_scripts(){
 	wp_enqueue_style( 'accelerate-style', get_template_directory_uri() . '/style.css' );
 	wp_enqueue_style( 'child-style', get_stylesheet_directory_uri() . '/style.css', array( 'accelerate-style' ));
 }
 add_action( 'wp_enqueue_scripts', 'accelerate_child_scripts' );
-// Custom post types functions
+
 function create_custom_post_types() {
-    register_post_type('case_studies',
+    register_post_type( 'case_studies',
         array(
             'labels' => array(
                 'name' => __( 'Case Studies' ),
@@ -28,35 +29,5 @@ function create_custom_post_types() {
             'rewrite' => array( 'slug' => 'case-studies' ),
         )
     );
-		register_post_type('services',
-        array(
-            'labels' => array(
-                'name' => __( 'services' ),
-                'singular_name' => __( 'service' )
-            ),
-            'public' => true,
-            'rewrite' => array( 'slug' => 'services' ),
-        )
-    );
 }
 add_action( 'init', 'create_custom_post_types' );
-// Defines custom markup for post comments
-function skillcrushstarter_comments($comment, $args, $depth) {
-	$comment  = '<li class="comment">';
-	$comment .=	'<header class="comment-head">';
-	$comment .= '<span class="comment-author">' . get_comment_author() . '</span>';
-	$comment .= '<span class="comment-meta">' . get_comment_date('m/d/Y') . '&emsp;|&emsp;' . get_comment_reply_link(array('depth' => $depth, 'max_depth' => 5)) . '</span>';
-	$comment .= '</header>';
-	$comment .= '<div class="comment-body">';
-	$comment .= '<p>' . get_comment_text() . '</p>';
-	$comment .= '</div>';
-	$comment .= '</li>';
-	echo $comment;
-}
-add_filter( 'body_class','accelerate_child_body_classes' );
-function accelerate_child_body_classes( $classes ) {
-  if (is_page('contact-us') ) {
-    $classes[] = 'contact-us';
-  }
-    return $classes;
-}
